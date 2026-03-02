4c5d77e0c915195a4702d33c2d58c8c9f867a989
Gulshan Kumar <43500848+g-kr@users.noreply.github.com>
2026-03-02 21:46:08 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,110 +1,95 @@
-const about = document.querySelector('#about')
-const contact = document.querySelector('#contact')
-const aboutContent = document.querySelector('#about-content')
-const contactContent = document.querySelector('#contact-content')
-const HOF=document.querySelector('#HOF')
-const projects=document.querySelector('#projects')
-const resume=document.querySelector('#resume')
-const HOFcontent=document.querySelector('#HOF-content')
-const projectContent=document.querySelector('#project-content')
-const resumeContent=document.querySelector('#resume-content')
+const about = document.querySelector('#about');
+const experience = document.querySelector('#experience');
+const projects = document.querySelector('#projects');
+const HOF = document.querySelector('#HOF');
+const resume = document.querySelector('#resume');
+const contact = document.querySelector('#contact');
+
+const aboutContent = document.querySelector('#about-content');
+const experienceContent = document.querySelector('#experience-content');
+const projectContent = document.querySelector('#project-content');
+const HOFcontent = document.querySelector('#HOF-content');
+const resumeContent = document.querySelector('#resume-content');
+const contactContent = document.querySelector('#contact-content');
+
+// Helper function to handle window background colors
+const windowOptions = {
+  onfocus: function () {
+    this.setBackground('#00aa00');
+  },
+  onblur: function () {
+    this.setBackground('#777');
+  }
+};
 
 about.addEventListener('click', () => {
-  const aboutBox = new WinBox({
+  new WinBox({
     title: 'About Me',
-    // modal: true,
-    width: '400px',
+    width: '450px',
     height: '400px',
     top: 50,
-    right: 50,
-    bottom: 50,
     left: 50,
     mount: aboutContent,
-    onfocus: function () {
-      this.setBackground('#00aa00')
-    },
-    onblur: function () {
-      this.setBackground('#777')
-    },
-  })
-})
+    ...windowOptions
+  });
+});
 
-contact.addEventListener('click', () => {
-  const contactBox = new WinBox({
-    title: 'Contact Me',
-    background: '#00aa00',
-    width: '400px',
-    height: '400px',
-    top: 150,
-    right: 50,
-    bottom: 50,
-    left: 250,
-    mount: contactContent,
-    onfocus: function () {
-      this.setBackground('#00aa00')
-    },
-    onblur: function () {
-      this.setBackground('#777')
-    },
-  })
-})
-
-HOF.addEventListener('click', () => {
-  const aboutBox = new WinBox({
-    title: 'Hall Of Fames',
-    // modal: true,
-    width: '400px',
-    height: '400px',
-    top: 250,
-    right: 50,
-    bottom: 50,
-    left: 350,
-    mount: HOFcontent,
-    onfocus: function () {
-      this.setBackground('#00aa00')
-    },
-    onblur: function () {
-      this.setBackground('#777')
-    },
-  })
-})
+experience.addEventListener('click', () => {
+  new WinBox({
+    title: 'Experience',
+    width: '500px',
+    height: '450px',
+    top: 100,
+    left: 100,
+    mount: experienceContent,
+    ...windowOptions
+  });
+});
 
 projects.addEventListener('click', () => {
-  const aboutBox = new WinBox({
+  new WinBox({
     title: 'Projects',
-    // modal: true,
-    width: '400px',
-    height: '400px',
-    top: 350,
-    right: 50,
-    bottom: 50,
-    left: 450,
+    width: '500px',
+    height: '450px',
+    top: 150,
+    left: 150,
     mount: projectContent,
-    onfocus: function () {
-      this.setBackground('#00aa00')
-    },
-    onblur: function () {
-      this.setBackground('#777')
-    },
-  })
-})
+    ...windowOptions
+  });
+});
+
+HOF.addEventListener('click', () => {
+  new WinBox({
+    title: 'Awards & Certifications',
+    width: '450px',
+    height: '400px',
+    top: 200,
+    left: 200,
+    mount: HOFcontent,
+    ...windowOptions
+  });
+});
 
 resume.addEventListener('click', () => {
-  const aboutBox = new WinBox({
+  new WinBox({
     title: 'Resume',
-    // modal: true,
     width: '400px',
-    height: '400px',
-    top: 450,
-    right: 50,
-    bottom: 50,
-    left: 550,
+    height: '250px',
+    top: 250,
+    left: 250,
     mount: resumeContent,
-    onfocus: function () {
-      this.setBackground('#00aa00')
-    },
-    onblur: function () {
-      this.setBackground('#777')
-    },
-  })
-})
+    ...windowOptions
+  });
+});
+
+contact.addEventListener('click', () => {
+  new WinBox({
+    title: 'Contact Me',
+    width: '400px',
+    height: '250px',
+    top: 300,
+    left: 300,
+    mount: contactContent,
+    ...windowOptions
+  });
+});
